12272906164791d81ea560a5316c2ec796db543e
robsaum <rob@boundlessliving.org>
2020-10-04 14:52:04 -0400

    Change shortcode trigger to yt

--- shortcode.php
+++ shortcode.php
@@ -12,6 +12,6 @@ function youtube_emebed_function( $atts, $content = null ) {
     return '<iframe src="https://www.youtube-nocookie.com/embed/'. $content .'?modestbranding=1&#038;rel=0&#038;showinfo=0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="" width="560" height="315" frameborder="0"></iframe>';
 }
 
-add_shortcode('youtube', 'youtube_emebed_function');
+add_shortcode('yt', 'youtube_emebed_function');
 
 ?>
